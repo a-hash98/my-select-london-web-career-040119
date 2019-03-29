b7ece59607e75de8578e33d05e13d905e341f2ab
Ay Has <ahashim983@yahoo.com>
2019-03-29 01:42:44 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,14 @@
 def my_select(collection)
+  new_collection = Array.new
+if block_given?
+  i = 0
+  while i < collection.length
+      new_collection.push(yield collection[i])
+    i += 1
+  end
+else
+  return "No block was given!"
+end
+return new_collection
  # your code here!
 end
